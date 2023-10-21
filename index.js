0ff9998157cd3e347d2474670a2f3ba8b30d7eef
softdevomar <omorfarukqqq@gmail.com>
2023-10-21 04:07:45 -0700

    simple  program added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("md omor faruk");
+ //area  calculation
+ 
+ var base = prompt("Enter your base: ");
+ var height = prompt("Enter your height : ");
+ var area = base * height;
+ document.write("Area  = "+area+"<br>");
+
+
+ //simple calculation
+ var num1 = prompt('Enter a first  nubmer :');
+ var num2 = prompt("Enter a second number :")
+ var sum = num1 + num2;
+ document.write("the result is : "+sum);
---
+++ kayword.js
@@ -0,0 +1,15 @@
+//javascript keywort total 26 
+//keyword name list  : break,case,catch, continue ,debugger , dafault ,delete,
+//do,else ,finally ,for ,function , if ,in ,instanceof , new return, switch ,
+//this, throw,try , typeof,var, void ,thile ,with ,
+
+
+//javaScritpt data type total type 4
+// 1.String
+//2.Number
+//3.Boolean
+//4.objects
+
+//String data type
+var name = "omar faruk";
+document.write(name);

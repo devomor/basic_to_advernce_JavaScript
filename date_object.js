c57d0aecf07a67564c1eb513d80d92d0934facf2
softdevomar <omorfarukqqq@gmail.com>
2023-10-21 05:54:36 -0700

    date object

---
+++ date_object.js
@@ -0,0 +1,10 @@
+var date = new Date();
+
+document.write(date.getFullYear());
+document.write(date.getDate());
+document.write(date.getDay());
+document.write(date.getHours());
+document.write(date.getMinutes());
+document.write(date.getTimezoneOffset());
+document.write(date.getTimezoneOffset());
+document.write(date.getUTCDate());

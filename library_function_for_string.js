1bbf3c33e5e83bdbf3fcfd0bda06b5304469b9e5
softdevomar <omorfarukqqq@gmail.com>
2024-06-13 23:07:37 -0700

    library funciton for string file add

--- library_function_for_string.js
+++ library_function_for_string.js
@@ -8,3 +8,29 @@ var text = "Omar Faruk";
 var len = text.length;
 //concatenation and write keyword using this in line;
 document.write("Number of character " + len);
+
+
+//javascript input method using the code
+var text1 = prompt("Enter you name");
+document.write(text1)
+document.write("Number of character" + text1.length);
+
+
+// character index value print
+var text2 = "Bangladesh"
+document.write(text2.charAt(2));
+
+//String convert to uppercase
+ document.write(text2.toUpperCase());
+
+ //String convert to lowerCase 
+ document.write(text2.toLowerCase());
+
+ //concatenation method using the code
+ var text3 = "is a beutifull country";
+ document.write(text2.concat(text3));
+
+ //slice mathod using the code
+document.write(text2.slice(0,2))//0 number index character cuting with 2 number index
+
+
